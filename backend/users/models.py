b0ed490b0b7bd485fbd88e824af1d075d47dc57a
dambre <lukas.dambr@gmail.com>
2018-10-20 14:57:17 +0300

    basemodel

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User as AuthUser
+
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    createdAt = models.DatetimeField(auto_now_add=True)
+    updatedAt = models.EmailField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel):
+    user = models.OneToOneField(AuthUser, on_delete=models.CASCADE)
+    email = models.EmailField(null=True, blank=True)
