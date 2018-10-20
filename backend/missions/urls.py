3a230e5248ae41be4268390ef2bdc913803d3362
Herqs <herqsss@gmail.com>
2018-10-20 20:11:04 +0300

    views in missions

--- backend/missions/urls.py
+++ backend/missions/urls.py
@@ -4,5 +4,10 @@ from missions import views
 
 urlpatterns = [
     path('t1/',views.all_missions),
-    path('t2/',views.all_missions)
+    path('t2/',views.all_missions),
+    path('<id>/',views.mission_by_id),
+    path('bycreator/<id>/',views.missions_by_creator),
 ]
+
+
+#TODO: FIX PATERN NAMES
--- backend/missions/views.py
+++ backend/missions/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core import serializers
+from django.forms.models import model_to_dict
+
 from missions.models import Mission
 
 
+
+
 def all_missions(request):
-    Missions = Mission.objects.values('uuid', 'title', 'description', 'creator', 'createdAt', 'updatedAt')
-    Missions.order_by('-updatedAt')
+    _missions = Mission.objects.values('uuid', 'title', 'description', 'creator', 'createdAt', 'updatedAt')
+    _missions.order_by('-updatedAt')
 
-    return JsonResponse(list(Missions), safe=False)
+    return JsonResponse(list(_missions), safe=False)
 
-# def list_users(request):
-#     users = User.objects.values('uuid', 'email', 'createdAt', 'updatedAt', 'userType')
-#     return JsonResponse(list(users), safe=False)
+def missions_by_creator(request,id):
+    _missions = Mission.objects.values('uuid', 'title', 'description', 'creator', 'createdAt', 'updatedAt')
+    _missions = _missions.filter(creator=id)
 
+    return JsonResponse(list(_missions), safe=False)
 
-def missions_by_creator(request):
-    Mission = Mission.objects.filter(creator=request.User)
+def mission_by_id(request,id):
+    _mission = Mission.objects.get(pk=id)
+    _mission = _mission.as_dict()
 
-
+    return JsonResponse(_mission, safe=False)
