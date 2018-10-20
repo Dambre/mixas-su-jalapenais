6313f511004458f1c3f1cbd2d4f71c953f979322
Herqs <herqsss@gmail.com>
2018-10-20 20:10:21 +0300

    missions now have as_dict

--- backend/missions/models.py
+++ backend/missions/models.py
@@ -8,3 +8,13 @@ class Mission(BaseModel):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def as_dict(self):
+        return {
+            'uuid': self.uuid,
+            'title': self.title,
+            'description': self.description,
+            'creator': self.creator_id,
+            'createdAt': self.createdAt,
+            'updatedAt': self.updatedAt,
+            }
